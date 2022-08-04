e91f4e3bd5755034014a8967df929b502ea4dae9
mkushimo01 <106722960+mkushimo01@users.noreply.github.com>
2022-08-04 03:00:40 -0600

    Update Sonar IP

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://52.91.78.168:9000/',
+  serverUrl: 'http://3.96.54.176:7000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
